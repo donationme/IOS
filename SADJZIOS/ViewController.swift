16aa51a2b477ec9e3cbb32fed842a07ad17a7e3d
Afshawn Lotfi <me@afshawn.org>
2018-12-04 15:10:17 -0500

    Initial Commit

---
+++ SADJZIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SADJZIOS
+//
+//  Created by Afshawn Lotfi on 12/4/18.
+//  Copyright © 2018 Afshawn Lotfi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SADJZIOSTests/SADJZIOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  SADJZIOSTests.swift
+//  SADJZIOSTests
+//
+//  Created by Afshawn Lotfi on 12/4/18.
+//  Copyright © 2018 Afshawn Lotfi. All rights reserved.
+//
+
+import XCTest
+@testable import SADJZIOS
+
+class SADJZIOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
